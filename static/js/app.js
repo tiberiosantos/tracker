7ef7784c700af1e4b9e13ed4b94cc1b97ed054ba
Tiberius <tiberius.santos@yahoo.com>
2020-03-29 23:05:29 -0300

    New version with patients tracking.
    - Application restructuring.
    - Obtaining the address coordinate information with ESRI.
    - Colored markers and icons to report the patient's condition.

--- static/js/app.js
+++ static/js/app.js
@@ -1,92 +1,146 @@
-function getFile(el, title) {
-  var link = null,
-    proxy = document.createElement('a');
-    date_prefix = new Date().toLocaleString().match(/\d+/g).join('');
+(() => {
+  'mode strict'
 
-  fetch(el.dataset.href, {
-    headers: new Headers({
-      'Origin': location.origin
-    }),
-    cors: true
-  }).then(function(response) {
-    return response.blob()
-  }).then(function(blob) {
-    if (link) URL.revokeObjectURL(link);
-    link = URL.createObjectURL(blob);
-    proxy.href = link;
-    proxy.download = `${title}_${date_prefix}.csv`;
-    proxy.click();
-  })
-}
-
-(function() {
-  var dataset = document.getElementById('download').dataset.href,
-    map = L.map('map'),
-    style = {
-      'color': '#8395a7',
-      'weight': 5,
-      'opacity': 0.65
-    }
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    'tileSize': 512,
-    'zoomOffset': -1,
-    'maxZoom': 16
-  }).addTo(map);
-  fetch('static/js/city.geojson')
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(data) {
-      var geojson = L.geoJSON(data, {'style': style}).addTo(map);
-      map.fitBounds(geojson.getBounds());
-    })
-
-  Papa.parse(dataset, {
-    downloadRequestHeaders: {
-      'Origin': location.origin
+  let data = {
+    download: document.getElementById('download').dataset.href,
+    layers: {
+      Leve: {
+        text: 'milds',
+        icon: 'fa-head-side-mask',
+        color: 'blue',
+        layer: L.layerGroup()
+      },
+      Grave: {
+        text: 'serious',
+        icon: 'fa-procedures',
+        color: 'orange',
+        layer: L.layerGroup()
+      },
+      Recuperado: {
+        text: 'recoveries',
+        icon: 'fa-male',
+        color: 'green',
+        layer: L.layerGroup()
+      },
+      Fatalidade: {
+        text: 'fatalities',
+        icon: 'fa-book-medical',
+        color: 'red',
+        layer: L.layerGroup()
+      }
     },
-    download: true,
-    header: true,
-    step: function(row) {
-      var data = row.data,
-        coord = data.GEOCODE.replace(/(^geo:|\?.*$)/g, '').split(',').map(Number),
-        numMarker = L.ExtraMarkers.icon({
-          'icon': 'fa-number',
-          'number': data.CONFIRMADOS,
-          'markerColor': 'orange'
-        }),
-        items = ['suspects', 'confirmations', 'recoveries', 'fatalities'],
-        headers = ['SUSPEITOS', 'CONFIRMADOS', 'RECUPERADOS', 'FATALIDADES'];
-
-      items.forEach(function(e) {
-        var sel = document.getElementById(e);
-        sel.textContent = (Number(sel.textContent) || 0) + Number(data[headers[items.indexOf(e)]]);
-      });
-
-      L.marker(coord, {icon: numMarker}).addTo(map)
-        .bindPopup(`<p class="caption">${data.UNIDADE}</p>
-        <p class="blue">
-          <i class="fas fa-microscope"></i>&nbsp;&nbsp;<strong>${data.SUSPEITOS}</strong> Casos Suspeitos
-        </p>
-        <p class="orange">
-          <i class="fas fa-virus"></i>&nbsp;&nbsp;<strong>${data.CONFIRMADOS}</strong> Casos Confirmados
-        </p>
-        <p class="green">
-          <i class="fas fa-heart"></i>&nbsp;&nbsp;<strong>${data.RECUPERADOS}</strong> Casos Recuperados 
-        </p>
-        <p class="red">
-          <i class="fas fa-cross"></i> &nbsp;&nbsp;<strong>${data.FATALIDADES}</strong> Casos Fatais
-        </p>`)
-        .on('click', function(e) {
-          map.flyTo(coord, 18);
-          this.openPopup();
-        })
-        .on('mouseover', function(e) {
-          this.openPopup();
-        });
+    style: {
+      color: '#8395a7',
+      weight: 5,
+      opacity: 0.65
     }
-  });
-})()
+  }
 
+  let methods = {
+    getData() {
+      return new Promise((resolve, reject) => {
+        Papa.parse(data.download, {
+          downloadRequestHeaders: {
+            'Origin': location.origin
+          },
+          download: true,
+          header: true,
+          complete: function(result) {
+            resolve(result.data)
+          }
+        })
+      })
+    },
+    getFile(el, title) {
+      var link = null,
+        proxy = document.createElement('a')
+      date_prefix = new Date().toLocaleString().match(/\d+/g).join('')
+
+      fetch(el.dataset.href, {
+        headers: new Headers({
+          Origin: location.origin
+        }),
+        cors: true
+      }).then(function(response) {
+        return response.blob()
+      }).then(function(blob) {
+        if (link) URL.revokeObjectURL(link)
+        link = URL.createObjectURL(blob)
+        proxy.href = link
+        proxy.download = `${title}_${date_prefix}.csv`
+        proxy.click()
+      })
+    },
+    getLayers() {
+      let layers = {}
+      Object.keys(data.layers).forEach(e => {
+        layers[e] = data.layers[e].layer
+      })
+      return layers
+    },
+    setMarkersLayer(sheet) {
+      return new Promise((resolve, reject) => {
+        sheet.forEach(row => {
+          L.esri.Geocoding.geocode()
+            .text(row.ENDEREÇO)
+            .run((err, results, response) => {
+              if (err) {
+                return
+              } else {
+                let icon = L.ExtraMarkers.icon({
+                  icon: data.layers[row.CASO].icon || 'fa-number',
+                  markerColor: data.layers[row.CASO].color || 'black',
+                  prefix: 'fas'
+                }),
+                  marker = L.marker(results.results[0].latlng, {icon: icon})
+                  .bindPopup(`<p class="caption"><i class="fas fa-user green"></i> ${row.PACIENTE}</p>
+                    <span><i class="fas fa-award blue"></i> <strong>Idade</strong>: ${row.IDADE}</span><br>
+                    <span><i class="fas fa-map-marker-alt blue"></i> <strong>Endereço</strong>: ${results.results[0].text}</span><br>
+                    <span><i class="fas fa-medkit blue"></i> <strong>Caso</strong>: ${row.CASO}</span><br>
+                    <span><i class="fas fa-stethoscope blue"></i> <strong>Situação</strong>: ${row.SITUAÇÃO}</span>
+                  `)
+                  .on('mouseover', function(e) {
+                    this.openPopup()
+                  });
+                if (icon.options.markerColor !== 'black') {
+                  marker.addTo(data.layers[row.CASO].layer)
+                }
+              }
+            })
+            let sel = document.getElementById(data.layers[row.CASO].text);
+            sel.textContent = (Number(sel.textContent) || 0) + 1;
+          })
+        resolve(true)
+      })
+    },
+    setBoundries(map) {
+      fetch('static/js/city.geojson')
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(data) {
+          let geojson = L.geoJSON(data, {style: data.style}).addTo(map);
+          map.fitBounds(geojson.getBounds());
+        })
+    },
+    init(scope) {
+      Object.keys(methods).forEach(m => scope[m] = this[m])
+      getData()
+        .then(data => setMarkersLayer(data))
+        .then(ok => {
+          if (ok) {
+            let tile = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attribution: '<a href="https://www.esri.com/">ESRI</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }),
+              layers = getLayers(),
+              map = L.map('map', {
+                layers: [tile].concat(Object.values(layers))
+              })
+            setBoundries(map)
+            L.control.layers({Topografia: tile}, layers).addTo(map);
+          }
+        })
+    }
+  }
+  return methods.init(this)
+}).call((this))
